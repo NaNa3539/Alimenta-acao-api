07c341f5f5cd24d6820a5d449bc4dd717f403987
Rafael Bernardes <rafael.bernardes@conlicitacao.com.br>
2026-05-13 12:29:29 -0300

    chore: configura porta dinâmica e script start para deploy

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..')));
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
